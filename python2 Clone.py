03d46e167da2a8259f493ef47d5fd838ca538204
lonewolf-313 <102681988+lonewolf-313@users.noreply.github.com>
2022-04-07 11:48:17 +0600

    Create python2 Clone.py
    cd Clone

---
+++ python2 Clone.py
@@ -0,0 +1,49 @@
+
+
+
+<html>
+    <head>
+<a id="home"/>
+<center><a href='/'><button class='header'>CyberTools24</button></a>
+</center><hr>
+</head>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+    <meta name="theme-color" content="tomato" />
+    <link rel="stylesheet" type="text/css" href="css/style.css"/>
+    <title>CyberTools24.cf - HomePage</title>
+
+</head>
+<body>
+    <div class="b">
+    <button class="btn" id="gvn">Generate Valid Numbers</button><b id="p1"></b>
+    <button class="btn" id="cmn">Cloning Mobile Number</button><b id="p2"></b>
+    <a href="/encoding-url.php"><button class="btn" id="eu">EncodeURL</button></a><b id="p3"></b>
+    <a href="/decoding-url.php"><button class="btn" id="du">DecodeURL</button></a><b id="p4"></b>
+    <a href="/login.php">Login</a>
+    </div>
+<script src="js/j_scripts.js"></script>
+<noscript>Javascript required: Your browser doesn't support javascript. Update your browser</noscript>
+
+
+<p>
+<b class="title">This is title</b><br></br>jgkrkkgekgeicgkckdgkyii8teiyetigkeyieigekyyktwkgggwkgeigeiteiteit3yiwiwitwruwurwurwurwurwurwurwitwuraruurauwieufwruwruwurwtw7euwufeyw8t3uwurwurruwurwurwtieiteyirieiteittwitwieiysiyshstiife
+This is some jgkrkkgekgeicgkckdgkyii8teiyetigkeyieigekyyktwkgggwkgeigeiteiteit3yiwiwitwruwurwurwurwurwurwurwitwuraruurauwieufwruwruwurwtw7euwufeyw8t3uwurwurruwurwurwtieiteyirieiteittwitwieiysiyshstiife This is some jgkrkkgekgeicgkckdgkyii8teiyetigkeyieigekyyktwkgggwkgeigeiteiteit3yiwiwitwruwurwurwurwurwurwurwitwuraruurauwieufwruwruwurwtw7euwufeyw8t3uwurwurruwurwurwtieiteyirieiteittwitwieiysiyshstiife This is some jgkrkkgekgeicgkckdgkyii8teiyetigkeyieigekyyktwkgggwkgeigeiteiteit3yiwiwitwruwurwurwurwurwurwurwitwuraruurauwieufwruwruwurwtw7euwufeyw8t3uwurwurruwurwurwtieiteyirieiteittwitwieiysiyshstiife This is some jgkrkk
+</p>
+
+<p><b class="title">This is title</b><br></br>jgkrkkgekgeicgkckdgkyii8teiyetigkeyieigekyyktwkgggwkgeigeiteiteit3yiwiwitwruwurwurwurwurwurwurwitwuraruurauwieufwruwruwurwtw7euwufeyw8t3uwurwurruwurwurwtieiteyirieiteittwitwieiysiyshstiife This is some jgkrkkgekgeicgkckdgkyii8teiyetigkeyieigekyyktwkgggwkgeigeiteiteit3yiwiwitwruwurwurwurwurwurwurwitwuraruurauwieufwruwruwurwtw7euwufeyw8t3uwurwurruwurwurwtieiteyirieiteittwitwieiysiyshstiife This is some jgkrkkgekgeicgkckdgkyii8teiyetigkeyieigekyyktwkgggwkgeigeiteiteit3yiwiwitwru
+</p>
+
+</body>
+    
+
+<br/><br/><br/><br/><br/><br/>
+<center>
+<footer class='somrat'>
+<a href="#home"><button class="jumpup">Jump to Top</button></a><br/><br/>
+™<b class="somrat">CyberTools24</b><br/>
+<a class='footer_link' href='/sitemap.html'>Sitemap</a> | <a class='footer_link' href='/contact.html'>Contact Us</a> | <a class='footer_link' href='/about.html'>About Us</a><br/>
+<b class="developedby">Developed by : </b><a class='josif' href='https://facebook.com/josifkhangg'>Md Josif Khan</a><br></br>
+</footer></center>
+</html>
+
